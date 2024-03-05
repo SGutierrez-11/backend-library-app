694efca2facbacc266e83c22fcc542bb77da4827
Santiago Gutierrez <santicoguvi02@gmail.com>
2024-03-05 01:24:42 -0500

    Adding the controller with the http methods to GET, ADD, UPDATE AND DELETE

--- backend-library-app/Program.cs
+++ backend-library-app/Program.cs
@@ -1,6 +1,15 @@
+using backend_library_app.Context;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Variable to connection DB
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+//Register services to Connection
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
