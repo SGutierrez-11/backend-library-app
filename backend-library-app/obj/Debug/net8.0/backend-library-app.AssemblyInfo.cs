f52dc8c1a6c2df321e2a4ebf22b6a262aff17467
Santiago Gutierrez <santicoguvi02@gmail.com>
2024-03-11 21:17:23 -0500

    Adding a new controller

--- backend-library-app/obj/Debug/net8.0/backend-library-app.AssemblyInfo.cs
+++ backend-library-app/obj/Debug/net8.0/backend-library-app.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-library-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d607049de79e130769bc47c63ffc6938d26af930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7e817e088378753e877ce93f0cf93d3dc052278")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-library-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-library-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
